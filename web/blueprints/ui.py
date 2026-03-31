3286d357c1927259a444d78ee862156870e84d64
Reedy Lab <admin@reedylab.com>
2026-03-31 19:10:19 +0000

    Remove Flask blueprints and Gunicorn config

--- web/blueprints/ui.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-ui_bp = Blueprint("ui", __name__, template_folder="../templates", static_folder="../static")
-
-
-@ui_bp.get("/")
-def home():
-    return render_template("ui.html", api_base="/api")
